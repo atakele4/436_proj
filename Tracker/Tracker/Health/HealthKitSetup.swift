13e2be678f08eab566cfa596bb1ad6f972f9b083
rushadantia <rushadantia@gmail.com>
2019-04-16 12:34:50 -0400

    health auth

---
+++ Tracker/Tracker/Health/HealthKitSetup.swift
@@ -0,0 +1,18 @@
+//
+//  HealthKitSetup.swift
+//  Tracker
+//
+//  Created by Rushad Antia on 4/16/19.
+//  Copyright © 2019 Harshil Patel. All rights reserved.
+//
+
+import Foundation
+
+
+class HealthKitSetup{
+    
+    class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+        
+    }
+    
+}
--- Tracker/Tracker/Health/HealthVC.swift
+++ Tracker/Tracker/Health/HealthVC.swift
@@ -11,4 +11,7 @@ import Foundation
 class HealthVC: UIViewController {
 
 
+    
+    
+    
 }
